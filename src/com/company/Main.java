da27be38aa7e478a9837c54b3b281d2d69dfc4d5
vinit-tiwari <vinit.tiwari@naukri.com>
2020-06-07 15:49:50 +0530

    Revert "Testing of Reset command in git"
    This reverts commit e4886a7ab84baab54dbee24ef14b2328b74c133b.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commiting the First Line through Command");
         System.out.println("Commiting the First Line through Command From Release");
-        System.out.println("Commiting the First Line through Git UI From Release");
     }
 }
