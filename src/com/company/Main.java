c5deba212bffe0139224da977761452c0d4cd3f7
vinit-tiwari <vinit.tiwari@naukri.com>
2020-06-07 15:52:50 +0530

    Commiting the second line through command line in master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commiting the First Line through Command");
         System.out.println("Commiting the First Line through Command From Master");
+        System.out.println("Commiting the Second Line through Command From Master");
     }
 }
