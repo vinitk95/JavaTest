e97a3c6706107abf1e5eaab7e4cf1f3a97adeff4
vinit-tiwari <vinit.tiwari@naukri.com>
2020-06-07 15:09:04 +0530

    commiting First changes in Master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Commiting the First Line through Command");
+        System.out.println("Commiting the First Line through Command From Master");
     }
 }
