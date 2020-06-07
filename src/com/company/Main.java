e4886a7ab84baab54dbee24ef14b2328b74c133b
vinit-tiwari <vinit.tiwari@naukri.com>
2020-06-07 15:46:59 +0530

    Testing of Reset command in git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commiting the First Line through Command");
         System.out.println("Commiting the First Line through Command From Release");
+        System.out.println("Commiting the First Line through Git UI From Release");
     }
 }
